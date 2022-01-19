dd450a85a4d26fc73020db6119b8470ec2527116
leechigu <78770548+leechigu@users.noreply.github.com>
2022-01-19 15:31:16 +0900

    프로그래머스 K번째수 URL : https://programmers.co.kr/learn/courses/30/lessons/42748?language=java
    더 쉬운 방법 있을텐데

--- src/SosuMaker.java
+++ src/SosuMaker.java
@@ -1,37 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class SosuMaker {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int x,y,z;
 
-
-    public boolean sosuCheck(int num){
-        int count = 0;
-        for(int i=1;i<=num;i++){
-            if(num%i==0)
-                count++;
-        }
-        if(count==2){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-
-
-    public int solution(int[] nums) {
-
-        int sum=0;
-        int count  =0;
-        for(int i=0;i<nums.length-2;i++){
-            for(int j = i+1;j<nums.length-1;j++){
-                for(int k= j+1;k<nums.length;k++){
-                    sum+=nums[i]+nums[j]+nums[k];
-                    if(sosuCheck(sum)){
-                        count++;
-                    }
-                    sum = 0;
-                }
+        for(int i = 0; i<commands.length;i++){
+            x = commands[i][0];
+            y = commands[i][1];
+            z = commands[i][2];
+            int[] arr = new int[y-x+1];
+            for(int j =x-1;j<y;j++){
+                arr[j-(x-1)]= array[j];
             }
+            Arrays.sort(arr);
+            answer[i] = arr[z-1];
         }
-        return count;
+        return answer;
     }
 }
