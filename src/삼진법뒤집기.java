5c7481b1f9d2c3df971dea76da3a0002e1b073b7
leechigu <78770548+leechigu@users.noreply.github.com>
2022-01-27 15:03:07 +0900

    프로그래머스 3진법 뒤집기

---
+++ src/삼진법뒤집기.java
@@ -0,0 +1,35 @@
+public class 삼진법뒤집기 {
+
+
+    public int solution(int n) {
+        int three_n=1;
+        int cnt=0;
+        while(true){
+            three_n =3*three_n;
+            cnt++;
+            if(n/three_n<1){
+                break;
+            }
+
+        }
+
+
+        three_n = three_n/3;
+        int []arr = new int[cnt+1];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = n/three_n;
+            n=n-(arr[i]*three_n);
+            three_n = three_n/3;
+        }
+        int x = 1;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum +=arr[i]*x;
+            x*=3;
+        }
+        return sum;
+    }
+
+
+
+}
