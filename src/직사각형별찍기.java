a77804abf1c767d48351a70b8cee876938e2e635
leechigu <78770548+leechigu@users.noreply.github.com>
2022-03-01 22:18:02 +0900

    직사각형별찍기

---
+++ src/직사각형별찍기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class 직사각형별찍기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int j=0;j<b;j++){
+            for(int i=0;i<a;i++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+}
