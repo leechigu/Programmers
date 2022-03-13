1f9dab48913a55217beea766b0194c26c8e65e16
leechigu <78770548+leechigu@users.noreply.github.com>
2022-03-13 16:31:07 +0900

    코딩테스트 연습 2019 카카오 개발자 겨울 인턴십 튜플

---
+++ src/튜플.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class 튜플 {
+    public static String[] strings;
+    public static void stringSlice(String s){
+
+        ArrayList<String> stringArrayList =new ArrayList<>();
+        String temp = "";
+        for(int i=1;i<s.length()-1;i++) {
+            temp += s.charAt(i);
+            if (s.charAt(i) == '}') {
+                stringArrayList.add(temp);
+                temp = "";
+                i++;
+            }
+        }
+        strings = new String[stringArrayList.size()];
+        for(int i=0;i<strings.length;i++){
+            temp = stringArrayList.get(i);
+            temp =temp.substring(1,temp.length()-1);
+            strings[i] = temp;
+        }
+    }
+    public static int[] getIntegersFromString(String st){
+        int size = 0;
+        for(int i=0;i<st.length();i++){
+            if(st.charAt(i) ==',')
+                size++;
+        }
+        int[] re = new int[size+1];
+        String temp ="";
+        int cnt=0;
+        for(int i=0;i<st.length();i++){
+            if(st.charAt(i)==','){
+                re[cnt] = Integer.parseInt(temp);
+                temp="";
+                cnt++;
+                continue;
+            }
+            temp += st.charAt(i);
+
+        }
+        re[re.length-1] = Integer.parseInt(temp);
+        return re;
+    }
+    public int[] solution(String s) {
+        stringSlice(s);
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length()-o2.length();
+            }
+        });
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int i=0;i<strings.length;i++){
+            int[] temp = getIntegersFromString(strings[i]);
+            for(int j=0;j<temp.length;j++){
+                if(!arrayList.contains(temp[j])){
+                    arrayList.add(temp[j]);
+                    break;
+                }
+            }
+        }
+        int[] answer = new int[arrayList.size()];
+        int cnt =0;
+        Iterator<Integer> it =  arrayList.iterator();
+        while(it.hasNext()){
+            answer[cnt] = it.next();
+            cnt++;
+        }
+        return answer;
+    }
+
+}
