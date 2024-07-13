2892b20a968b9197142cab5bd6b7925d3fc5de66
leechigu <78770548+leechigu@users.noreply.github.com>
2024-07-13 17:23:53 +0900

    다시 영차영차 하자

--- src/프로그래머스/LEVEL3/연속펄스부분수열의합.java
+++ src/프로그래머스/LEVEL3/연속펄스부분수열의합.java
@@ -1,17 +1,31 @@
 package 프로그래머스.LEVEL3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 연속펄스부분수열의합 {
-    public long answer = 0;
-
-
 
     public long solution(int[] sequence) {
 
 
+        long answer = 0;
+        long case1 = 0;
+        long case2 = 0;
+        int x = 1;
+        for(int cur :  sequence){
 
+            //1 -1 1 ...
+            case1 += cur * x;
+            //-1 1 -1 ...
+            case2 += cur * -x;
 
+            case1 = Math.max(0,case1);
+            case2 = Math.max(0,case2);
 
+            answer = Math.max(answer,Math.max(case1,case2));
 
+            x = -x;
+        }
         return answer;
     }
 }
